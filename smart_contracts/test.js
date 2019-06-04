601b4f08e9f87c4e8f77b8df79d90e88c3eefba4
Martin Kuzdowicz <m.kuzdowicz@elsevier.com>
2019-06-04 16:04:14 +0100

    data transaction

--- smart_contracts/test.js
+++ smart_contracts/test.js
@@ -14,3 +14,15 @@ it('transfer', async function() {
     await waitForTx(tx.id)
     console.log(JSON.stringify(tx));
 })
+
+it('data transaction', async function(){
+    let aboutObject = {
+        "name": "Martin Kuzdowicz",
+        "occupation": "Software Engineer"
+    }
+    let aboutStr = JSON.stringify(aboutObject)
+    let payload = {data: [{key: "web-3-online-course-user-data",value: aboutStr}]}
+
+    let tx = await broadcast(data(payload, accaountASeed))
+    await waitForTx(tx.id)
+})
